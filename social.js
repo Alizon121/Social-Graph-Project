11a5e2a37e56afbe5e9a43c25857e033ac2fe77d
Andrew <andrew.lizon12@gmail.com>
2024-05-30 08:25:35 -0700

    getFollowers method complete

--- social.js
+++ social.js
@@ -56,14 +56,32 @@ else return false
 
   getFollowers(userID) {
     // return set containing all followers of userID
-    // Who follows userID?
+    // Who follows userID1? No one.
+    // How is a follower represented?
+    // iterate through your this.follows object to find each user that follows your userID user
 
-return console.log(userID)
-  
+
+  // userId1 follows User 2, User 2 follows no one.
+  // userdID 1 is followed by no one -> no followers
+      //follower :  followed
+  //  { '1': Set(1) { 2 }, '2': Set(0) {} }
+    // console.log(this.follows[userID])
+
+    let followers = new Set()
+
+    for (let el in this.follows) {
+      if (this.follows[el].has(userID)) { // If object contains the userID, then return follower
+        followers.add(Number(el)) // Need to convert a string into an int
+      }
+    }
+return followers
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here 
+    // return an array containing a list of recommended users to follow
+    // Use the follows to make recs for a user
+
+
   }
 }
 
