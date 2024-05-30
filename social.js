14f8047d5fc8b65370d80d53a7b0ac55419a0cc4
Andrew <andrew.lizon12@gmail.com>
2024-05-30 10:37:19 -0700

    Completed getRecommendedFollows function

--- social.js
+++ social.js
@@ -80,9 +80,52 @@ return followers
   getRecommendedFollows(userID, degrees) {
     // return an array containing a list of recommended users to follow
     // Use the follows to make recs for a user
+    // degrees arg represents the distance that the algo will search for recommended follows
+    // Use the last element in an array to continue with the degrees/recommended follows
+    // We need to traverse through the graph a certain amount of iterations (e.g. degree)
+
+    // This is the same at the shortest path problem
+    let queue = [[userID]]
+    let visited = new Set()
+    visited.add(userID)
+
+    let recommended = []
+
+    // {
+    //   '1': Set(1) { 2 },
+    //   '2': Set(1) { 3 },
+    //   '3': Set(2) { 4, 5 },
+    //   '4': Set(2) { 1, 2 },
+    //   '5': Set(1) { 6 },
+    //   '6': Set(0) {}
+    // }
+    // console.log(this.follows)
+// We need a way of only doing degrees iterations
+    while(queue.length > 0) {
+      let users = queue.shift()
+      let currentUsers = users[users.length-1]
+
+    
+      // we need a way to access the follows of next user
 
 
-  }
+      if (users.length > 2 && users.length <= degrees + 2) {
+          if (!this.follows[userID].has(currentUsers)) { // If the user is not following the other user already
+            recommended.push(currentUsers) // add 'recommendations' to the user array
+          } 
+      }
+
+      for (let el of this.follows[currentUsers]) { // iterate through users: this.follows[userId] gives only one user
+      if (!visited.has(el)) {
+        visited.add(el) // Make a list of users that have been visited.
+        queue.push(users.concat([el])) // add users to queue that have not been visited.
+      }
+
+      }
+
+    }
+    return recommended
+
 }
-
+}
 module.exports = SocialNetwork;
